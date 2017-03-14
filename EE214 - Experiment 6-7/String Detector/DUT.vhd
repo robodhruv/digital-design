52dac339ecfb68abe69da2306d68aa4711c93374
PrieureDeSion <dhruv.ilesh@gmail.com>
2017-03-15 00:03:58 +0530

    Lab7 Report Done

--- EE214 - Experiment 6-7/String Detector/DUT.vhd
+++ EE214 - Experiment 6-7/String Detector/DUT.vhd
@@ -6,7 +6,6 @@ entity DUT is
 	output_vector: out std_ulogic_vector(0 downto 0));
 end entity;
 
-
 architecture WRAP of DUT is
 component bomber is
   port(x: in std_ulogic_vector(4 downto 0);
@@ -32,12 +31,9 @@ component terrorist is
 		s: out std_ulogic);
 end component;
 
-
   signal res, bs, gs, ks, ts, clk: std_ulogic;
   signal x: std_ulogic_vector(4 downto 0);
-
 begin
-   
 	res <= input_vector(6);	
 	clk <= input_vector(5);
 
@@ -48,5 +44,4 @@ begin
 	G: gunman port map (reset => res, x => x, s => gs, clk => clk);
 	K: knife_hurler port map (reset => res, x => x, s => ks, clk => clk);
 	T: terrorist port map (reset => res, x => x, s => ts, clk => clk);
-
 end WRAP;
--- EE214 - Experiment 6/Up Down Counter/compile_ghdl.sh
+++
@@ -1,5 +0,0 @@
-ghdl -i EE224.vhd
-ghdl -i LogicNetwork.vhd
-ghdl -i DUT.vhd
-ghdl -i Testbench.vhd
-ghdl -m Testbench
